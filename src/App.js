715a426c97b708fd3c67caa8ba2719396218088b
jason <jason@vintrace.com>
2019-08-13 12:59:56 +1000

    55. Outputting Lists

--- src/App.js
+++ src/App.js
@@ -56,19 +56,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={this.swtichNameHandler.bind(this, 'Maxie!!!!')} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.swtichNameHandler.bind(this, 'Maxie!!!!')}
-            changed={this.nameChangeHandler}>Hobbies: Racing</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.swtichNameHandler.bind(this, 'Maxie!!!!')} />
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age} />
+          })}
         </div>
       );
     }
