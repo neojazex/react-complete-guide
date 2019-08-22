b0aee2902c4d4dd28b81aab4942a2c3ff3c3a558
jason <jason@vintrace.com>
2019-08-22 13:12:16 +1000

    65. Setting Styles Dynamically

--- src/App.js
+++ src/App.js
@@ -46,7 +46,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1x solid blue',
       padding: '8px',
@@ -66,6 +67,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
 
 
