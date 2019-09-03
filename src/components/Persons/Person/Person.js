dfa1660a48c119f8a7d1114eae5bb30a481f027a
jason <jason@vintrace.com>
2019-09-04 07:44:24 +1000

    101. Rendering Adjacent JSX Elements - array example

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,13 @@ import classes from './Person.module.css';
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
-        return (
-            <div className={classes.Person}>
-            <p onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age}</p>
-            <p>{this.props.children}</p>
-            <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
-        );
+        return [
+            // <div className={classes.Person}>
+            <p key="i1" onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age}</p>,
+            <p key="i2">{this.props.children}</p>,
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+            // </div>
+        ];
     }
 }
 export default Person;
