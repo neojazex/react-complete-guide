adf367e4a0cafccea762cfab7a4599463ea24aa2
jason <jason@vintrace.com>
2019-09-04 07:53:37 +1000

    101. Rendering Adjacent JSX Elements - auxillary pattern

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 
 import classes from './Person.module.css';
 
+import Aux from "../../../hoc/Auxillary"
+
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
-        return [
-            // <div className={classes.Person}>
-            <p key="i1" onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age}</p>,
-            <p key="i2">{this.props.children}</p>,
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-            // </div>
-        ];
+        return (//className={classes.Person}
+            <Aux> 
+                <p onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age}</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </Aux>
+        );
     }
 }
 export default Person;
